b3d1733ce2ba2bf8830d834e6355c5d8b8cb6eb3
Barkley <barkley.santo@gmail.com>
2023-02-07 13:30:54 +0800

    basic styled and positioning for App.jsx

--- fi-banking/src/App.jsx
+++ fi-banking/src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './styles'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}` }>
+            Navbar
+        </div>
+     </div>
+    
+    <div className={ `bg-secondary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero component
+      </div>
+    </div>
+
+    <div className={ `bg-secondary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* render all components */}
+        Stats
+        Business
+        Billing
+        CardDeal
+        Tesimonials
+        Clients
+        Footer
+      </div>
+    </div>
+
+    </div>
+)
+
 
 export default App
