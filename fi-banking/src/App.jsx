74c64bc93101611ed49cf8b2546626e5b92549c6
Barkley <barkley.santo@gmail.com>
2023-02-10 14:53:23 +0800

    create index.js in components to import all components. import that file into App.js. Import some assets into NavBar.js

--- fi-banking/src/App.jsx
+++ fi-banking/src/App.jsx
@@ -1,30 +1,47 @@
 import React from 'react'
 import styles from './styles'
+import { 
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from './components'
+ 
+
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}` }>
-            Navbar
+            <Navbar />
         </div>
      </div>
     
     <div className={ `bg-secondary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero component
+        <Hero />
       </div>
     </div>
 
     <div className={ `bg-secondary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         {/* render all components */}
-        Stats
-        Business
-        Billing
-        CardDeal
-        Tesimonials
-        Clients
-        Footer
+        <Stats/>
+        <Business/>
+        <Billing/>
+        <CardDeal/>
+        <Testimonials/>
+        <Clients/>
+        <Footer/>
       </div>
     </div>
 
---
+++ fi-banking/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import {close, logo, menu} from '../assets'
+
+const Navbar = () => {
+  return (
+    <img src={logo} alt="" />
+  )
+}
+
+export default Navbar
