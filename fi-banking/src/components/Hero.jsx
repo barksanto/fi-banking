d6f08c69550a28a8fbf8dcfe633a155d22a0e35f
Barkley <barkley.santo@gmail.com>
2023-02-13 17:14:29 +0800

    complete Stats section/component - layout and styles

--- fi-banking/src/components/Hero.jsx
+++ fi-banking/src/components/Hero.jsx
@@ -19,11 +19,29 @@ const Hero = () => {
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
             THE NEXT <br className="sm:block hidden " />{" "}
             <span className="text-gradient">GENERATION</span>{" "}
-            PAYMENT METHOD
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0"><GetStarted/></div>
         </div>
+          <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-[full]">
+            Payment Method
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          <span className="text-gradient">Fi Banking</span> is a new way to pay with your phone. Our team of experts use a methodology to identify the credit cards that most fit your needs. we examine your credit score, income, and other factors to find the best credit cards for you.
+        </p>
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
+        <div className='absolute z-[1] w-[80%] h-[80%] rounded-[full] bottom-[40] white__gradient'/>
+        <div className='absolute z-[0] w-[50%] h-[50%] right-[20] bottom-[20] rounded-[full] bottom-[40] blue__gradient'/>
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted/>
+      </div>
+
+      
     </section>
   )
 }
--- fi-banking/src/constants/index.js
+++ fi-banking/src/constants/index.js
@@ -88,17 +88,17 @@ export const feedback = [
 export const stats = [
   {
     id: "stats-1",
-    title: "User Active",
+    title: "Active Users",
     value: "3800+",
   },
   {
     id: "stats-2",
-    title: "Trusted by Company",
+    title: "Companies Served",
     value: "230+",
   },
   {
     id: "stats-3",
-    title: "Transaction",
+    title: "Transactions",
     value: "$230M+",
   },
 ]
