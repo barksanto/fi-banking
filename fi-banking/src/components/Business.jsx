6c9ff3a269c23d1ee169cbc78ec9393dba9b20d0
Barkley <barkley.santo@gmail.com>
2023-02-14 13:00:34 +0800

    added testimonials with quotes and user images

--- fi-banking/src/components/Business.jsx
+++ fi-banking/src/components/Business.jsx
@@ -26,6 +26,7 @@ const Business = () => {
 
   return (
     <section id="features" className={`${layout.section}`}>
+
       <div className={`${layout.sectionInfo}`}>
         <h2 className={`${styles.heading2}`}>You handle the business <br className="sm:block hidden" /> We'll handle the money.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
@@ -39,6 +40,7 @@ const Business = () => {
           ))
         }
       </div>
+      
     </section>
   )
 }
--- fi-banking/src/components/CardDeal.jsx
+++ fi-banking/src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../styles';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
+
+const CardDeal = () => (
+  <section className={`${layout.section}`}>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={`${styles.heading2} `}>Get your card deal <br className={`sm:block hidden`} /> in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>It can be a hassle to find a card that best suits your needs. Not anymore, we've got your back this time.</p>
+      <Button styles="mt-10"/>
+    </div>
+
+    <div className={`${layout.sectionImg}`}>
+      <img src={card} alt="card_images" className='w-[100%] h-[100%]' />
+    </div>
+  </section>
   )
-}
+
 
 export default CardDeal
