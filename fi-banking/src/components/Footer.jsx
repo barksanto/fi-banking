46e491abeb690658b263e59990f962ad9adba241
Barkley <barkley.santo@gmail.com>
2023-02-14 18:44:44 +0800

    updated logo

--- fi-banking/src/components/Footer.jsx
+++ fi-banking/src/components/Footer.jsx
@@ -11,9 +11,9 @@ import { footerLinks, socialMedia } from "../constants";
         <img
           src={logo}
           alt="hoobank"
-          className="w-[266px] h-[72.14px] object-contain"
+          className="w-[266px] h-[120px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+        <p className={`${styles.paragraph} mt-4 max-w-[312px] text-center`}>
           A new way to make the payments easy, reliable and secure.
         </p>
       </div>
@@ -39,6 +39,18 @@ import { footerLinks, socialMedia } from "../constants";
          }
        </div>
      </div>
+
+     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]">
+       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">Copyright 2023 FiBank. All Rights Reserved.</p>
+       <div className="flex flex-row md:mt-0 mt-6">
+         {
+           socialMedia.map((social, index) => (
+             <img key={social.id} src={social.icon} alt={social.id} className={`${index !== socialMedia.length -1 ? "mr-6" : "mr-0"} w-[21px] h-[21px] object-contain cursor-pointer `} />
+           ))
+         }
+       </div>
+     </div>
+
     </section>
   )
 
